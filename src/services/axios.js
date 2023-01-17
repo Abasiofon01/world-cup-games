0802e85d2524ac0deea35ed802eb64bd84a7ea12
Ndianabasi Udonkang <ndianabasi@furnish.ng>
2023-01-17 19:06:25 +0100

    feat: setup .env to hold API base url

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
-export const http = axios.create({baseURL: 'http://127.0.0.1:3333'});
+export const http = axios.create({
+  baseURL: import.meta.env.VUE_APP_API_BASE_URL
+});
 
 http.interceptors.response.use(
-  (response) => {
+  response => {
     return response;
   },
-  (error) => {
+  error => {
     console.log(error.response);
 
     return Promise.reject(error);
