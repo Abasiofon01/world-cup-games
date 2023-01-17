e2c0dddfd7431d9cfd4b4833c050c1979b97d716
Ndianabasi Udonkang <ndianabasi@furnish.ng>
2023-01-17 20:11:37 +0100

    feat: add route for creating new group

--- src/router/index.js
+++ src/router/index.js
@@ -1,40 +1,60 @@
-import Teams from '../views/Teams.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import Teams from "../views/Teams.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
-
-    {
-        path: '/teams',
-        name: 'Teams',
-        component: Teams
-    },
-    {
-        path: '/groups',
-        name: 'Groups',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/Groups.vue')
-    },
-    {
-        path: '/matches',
-        name: 'Matches',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/Matches.vue')
-    },
-    {
-        path: '/standings',
-        name: 'Standings',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/Standings.vue')
+  {
+    path: "/",
+    name: "home",
+    redirect() {
+      return { name: "Teams" };
     }
-]
+  },
+
+  {
+    path: "/teams",
+    name: "Teams",
+    component: Teams
+  },
+
+  {
+    path: "/groups",
+    name: "Groups",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Groups.vue"),
+    children: [
+      {
+        path: "new",
+        name: "new_group",
+        component: () => import("../views/GroupForm.vue")
+      }
+    ]
+  },
+
+  {
+    path: "/matches",
+    name: "Matches",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Matches.vue")
+  },
+
+  {
+    path: "/standings",
+    name: "Standings",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Standings.vue")
+  }
+];
 const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
-export default router
+  history: createWebHistory(),
+  routes
+});
+export default router;
