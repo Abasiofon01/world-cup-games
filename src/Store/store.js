5d28e579aa7ff1ced160b977a4879ad656aecc3a
Abasiofon01 <abasiofonudonkang@gmail.com>
2023-01-18 17:52:58 +0100

    feat: create actions for fetching, editing and creating teams

--- src/Store/store.js
+++ src/Store/store.js
@@ -2,57 +2,56 @@ import { createStore } from "vuex";
 import { http } from "../services/axios";
 
 export const store = createStore({
-  state: {
-    posts: [],
-    teams: []
-  },
-  getters: {
-    posts: state => {
-      return state.posts;
-    },
-    teams: state => {
-      return state.teams;
-    }
-  },
-  mutations: {
-    SET_ITEMS(state, posts) {
-      state.posts = posts;
-    },
-    SET_TEAMS(state, teams) {
-      state.teams = teams;
-    }
-  },
+  //   state: {
+  //     group: [],
+  //   },
+
+  //   getters: {
+  //     group: state => {
+  //       return state.group;
+  //     },
+  //   },
+
+  //   mutations: {
+  //     GET_GROUP(state, group) {
+  //       state.group = group;
+  //     },
+
+  //   },
+
   actions: {
-    async loadPosts({ commit }) {
-      return http
-        .get("http://jsonplaceholder.typicode.com/posts")
-        .then(response => {
-          commit("SET_ITEMS", response.data);
-          return response.data;
-        });
-    },
     async createGroup(ctx, { form }) {
-      return http.post("groups", form).then(response => {
+      return http.post("groups", form).then((response) => {
         return response.data;
       });
     },
     async editGroup(ctx, { form, id }) {
-      return http.put(`groups/${id}`, form).then(response => {
+      return http.put(`groups/${id}`, form).then((response) => {
         return response.data;
       });
     },
     async fetchGroup(ctx, id) {
-      return http.get(`groups/${id}`).then(response => {
+      return http.get(`groups/${id}`).then((response) => {
+        //   commit("GET_GROUP", response.data.data);
         return response.data.data;
       });
     },
-    async getTeams({ commit }) {
-      try {
-        const response = await http.get("teams");
-        commit("SET_TEAMS", response.data);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  }
+    
+    async createTeam(ctx, { form }) {
+      return http.post("teams", form).then((response) => {
+        return response.data;
+      });
+    },
+    async editTeam(ctx, { form, id }) {
+      return http.put(`teams/${id}`, form).then((response) => {
+        return response.data;
+      });
+    },
+    async fetchTeam(ctx, id) {
+      return http.get(`teams/${id}`).then((response) => {
+        //   commit("GET_TEAM", response.data.data);
+        return response.data.data;
+      });
+    },
+  },
 });
