376705beaa6e9a05925589c0b0a760ad0110ef51
Abasiofon01 <abasiofonudonkang@gmail.com>
2023-01-19 11:08:09 +0100

    style: improve style and responsiveness on mobile.

--- src/views/GroupForm.vue
+++ src/views/GroupForm.vue
@@ -10,55 +10,77 @@
 </template>
 
 <script>
-  import Layout from "../Layout/Layout.vue";
-  import { mapActions } from "vuex";
+import Layout from "../Layout/Layout.vue";
+import { mapActions } from "vuex";
 
-  export default {
-    name: "GroupForm",
+export default {
+  name: "GroupForm",
 
-    data() {
-      return {
-        form: {
-          name: ""
+  data() {
+    return {
+      form: {
+        name: "",
+      },
+    };
+  },
+
+  components: {
+    Layout,
+  },
+
+  computed: {
+    isEditMode() {
+      return this.$route.name === "edit_group";
+    },
+
+    headerTitle() {
+      return this.isEditMode ? "Edit Group" : "Create Group";
+    },
+  },
+
+  methods: {
+    async onSubmit() {
+      const submitFn = this.isEditMode ? this.editGroup : this.createGroup;
+
+      await submitFn({ form: this.form, id: this.$route.params?.id }).then(
+        (response) => {
+          console.log(response);
         }
-      };
+      );
     },
 
-    components: {
-      Layout
-    },
+    ...mapActions(["createGroup", "fetchGroup", "editGroup"]),
+  },
 
-    computed: {
-      isEditMode() {
-        return this.$route.name === "edit_group";
-      },
-
-      headerTitle() {
-        return this.isEditMode ? "Edit Group" : "Create Group";
-      }
-    },
-
-    methods: {
-      async onSubmit() {
-        const submitFn = this.isEditMode ? this.editGroup : this.createGroup;
-
-        await submitFn({ form: this.form, id: this.$route.params?.id }).then(
-          response => {
-            console.log(response);
-          }
-        );
-      },
-
-      ...mapActions(["createGroup", "fetchGroup", "editGroup"])
-    },
-
-    async created() {
-      if (this.isEditMode) {
-        await this.fetchGroup(this.$route.params.id).then(data => {
-          console.log(data);
-          this.form.name = data.name;
-        });
-      }
+  async created() {
+    if (this.isEditMode) {
+      await this.fetchGroup(this.$route.params.id).then((data) => {
+        console.log(data);
+        this.form.name = data.name;
+      });
     }
-  };
+  },
+};
 </script>
+
+<style scoped>
+.form-wrapper {
+  margin-top: 18px;
+}
+
+form {
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  justify-content: center;
+}
+
+form label {
+  color: white;
+}
+
+form input {
+  border-radius: 4px;
+  padding: 2px 6px;
+}
+</style>
--- src/views/TeamForm.vue
+++ src/views/TeamForm.vue
@@ -3,20 +3,28 @@
     <template #title> {{ headerTitle }} </template>
 
     <form>
-      <div class="group-name-wrapper">
-        <label for="group_name">Team Name</label>
-        <input v-model="form.name" id="group_name" type="text" />
+      <div class="name-input-wrapper">
+        <label for="flag">Flag</label>
+        <input v-model="form.flag" id="flag" type="text" />
       </div>
 
-      <select v-model="form.group_id">
-        <option
-          v-for="group in groupsForSelect"
-          :key="`group_${group.id}`"
-          :value="group.id"
-        >
-          {{ group.name }}
-        </option>
-      </select>
+      <div class="name-input-wrapper">
+        <label for="team_name">Team Name</label>
+        <input v-model="form.name" id="team_name" type="text" />
+      </div>
+
+      <div class="name-input-wrapper">
+        <label for="group_name">Group</label>
+        <select v-model="form.group_id">
+          <option
+            v-for="group in groupsForSelect"
+            :key="`group_${group.id}`"
+            :value="group.id"
+          >
+            {{ group.name }}
+          </option>
+        </select>
+      </div>
     </form>
 
     <button id="btn" @click.prevent="onSubmit">{{ headerTitle }}</button>
@@ -24,112 +32,123 @@
 </template>
 
 <script>
-  import Layout from "../Layout/Layout.vue";
-  import { mapActions } from "vuex";
+import Layout from "../Layout/Layout.vue";
+import { mapActions } from "vuex";
 
-  export default {
-    name: "TeamForm",
+export default {
+  name: "TeamForm",
 
-    data() {
-      return {
-        form: {
-          name: "",
-          group_id: ""
-        },
-        groupsForSelect: []
-      };
-    },
-
-    components: {
-      Layout
-    },
-
-    computed: {
-      isEditMode() {
-        return this.$route.name === "edit_team";
+  data() {
+    return {
+      form: {
+        name: "",
+        group_id: "",
+        flag: "",
       },
+      groupsForSelect: [],
+    };
+  },
 
-      headerTitle() {
-        return this.isEditMode ? "Edit Team" : "Create Team";
-      }
+  components: {
+    Layout,
+  },
+
+  computed: {
+    isEditMode() {
+      return this.$route.name === "edit_team";
     },
 
-    methods: {
-      async onSubmit() {
-        const submitFn = this.isEditMode ? this.editTeam : this.createTeam;
-
-        await submitFn({ form: this.form, id: this.$route.params?.id }).then(
-          response => {
-            console.log(response);
-          }
-        );
-      },
-
-      ...mapActions([
-        "fetchGroup",
-        "createTeam",
-        "fetchTeam",
-        "editTeam",
-        "fetchAllGroups"
-      ])
+    headerTitle() {
+      return this.isEditMode ? "Edit Team" : "Create Team";
     },
+  },
 
-    async created() {
-      const allGroupsPromise = this.fetchAllGroups();
-      const fetchTeamPromise = this.fetchTeam(this.$route.params.id);
+  methods: {
+    async onSubmit() {
+      const submitFn = this.isEditMode ? this.editTeam : this.createTeam;
 
-      /** @typedef {[{data: Array<{id: string; name: string}>}, {name: string; id: string; group_id: string}]} ResultArray */
-
-      await Promise.all([
-        allGroupsPromise,
-        this.isEditMode && fetchTeamPromise
-      ]).then((/** @type {ResultArray} */ [allGroupsData, teamData]) => {
-        console.log(allGroupsData);
-        this.groupsForSelect = allGroupsData.data.map(group => ({
-          id: group.id,
-          name: group.name
-        }));
-
-        if (this.isEditMode) {
-          console.log(teamData);
-          this.form.name = teamData.name;
-          this.form.group_id = teamData.group_id;
+      await submitFn({ form: this.form, id: this.$route.params?.id }).then(
+        (response) => {
+          console.log(response);
         }
-      });
-    }
-  };
+      );
+    },
+
+    ...mapActions([
+      "fetchGroup",
+      "createTeam",
+      "fetchTeam",
+      "editTeam",
+      "fetchAllGroups",
+    ]),
+  },
+
+  async created() {
+    const allGroupsPromise = this.fetchAllGroups();
+    const fetchTeamPromise = this.fetchTeam(this.$route.params.id);
+
+    /** @typedef {[{data: Array<{id: string; name: string}>}, {name: string; id: string; group_id: string}]} ResultArray */
+
+    await Promise.all([
+      allGroupsPromise,
+      this.isEditMode && fetchTeamPromise,
+    ]).then((/** @type {ResultArray} */ [allGroupsData, teamData]) => {
+      console.log(allGroupsData);
+      this.groupsForSelect = allGroupsData.data.map((group) => ({
+        id: group.id,
+        name: group.name,
+      }));
+
+      if (this.isEditMode) {
+        console.log(teamData);
+        this.form.flag = teamData.flag;
+        this.form.name = teamData.name;
+        this.form.group_id = teamData.group_id;
+      }
+    });
+  },
+};
 </script>
 
-<style scoped>
-  .group-name-wrapper {
-    color: white;
-  }
+<style lang="css" scoped>
+.form-wrapper {
+  margin-top: 18px;
+}
 
-  .group-name-wrapper input {
-    margin-left: 10px;
-    border-color: none;
-    color: #830542;
-  }
+.name-input-wrapper {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  color: white;
+  width: 100%;
+}
+
+.name-input-wrapper input,
+select {
+  margin: 5px 0 0 10px;
+  padding: 2px 6px;
+  border: none;
+  color: #830542;
+  border-radius: 4px;
+}
+form {
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  justify-content: center;
+}
+@media (max-width: 500px) {
   form {
-    display: flex;
     flex-direction: column;
-    gap: 10px;
-    align-items: center;
   }
+}
 
-  select {
-    padding: 2px 6px;
-    border: none;
-    color: #830542;
-  }
-
-  #btn {
-    padding: 5px 20px;
-    color: #830542;
-    background-color: white;
-    border-radius: 4px;
-    display: block;
-    margin-bottom: 18px;
-    margin: 10px auto;
-  }
+#btn {
+  padding: 5px 20px;
+  color: #830542;
+  background-color: white;
+  border-radius: 4px;
+  display: block;
+  margin: 20px auto;
+}
 </style>
