f2bcb31eeb54e953f6c16cd27ac7f8d635e7683d
Abasiofon01 <abasiofonudonkang@gmail.com>
2023-01-19 22:29:02 +0100

    feat: add back btn and emit click event. Style back btn.

--- src/Layout/Layout.vue
+++ src/Layout/Layout.vue
@@ -4,6 +4,19 @@
       <slot name="title"></slot>
     </h1>
 
+    <div class="icon-wrapper" @click="goBack" v-if="showBackBtn">
+      <svg
+        id="back_icon"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M1.02686 11.9928L5.2623 16.2426L6.67889 14.8308L4.85754 13.0032L22.9729 13.0011L22.9727 11.0011L4.85297 11.0032L6.68848 9.17392L5.27665 7.75732L1.02686 11.9928Z"
+        />
+      </svg>
+    </div>
+
     <button id="btn" v-if="showNewBtn" type="button" @click="createNew">
       New
     </button>
@@ -25,11 +38,18 @@ export default {
       type: Boolean,
       default: true,
     },
+    showBackBtn: {
+      type: Boolean,
+      default: true,
+    },
   },
 
   methods: {
     createNew() {
       this.$emit("create-new");
+    },
+    goBack() {
+      this.$emit("go-back");
     },
   },
 };
@@ -44,5 +64,20 @@ export default {
   align-self: center;
   display: inline;
   margin-bottom: 18px;
+}
+
+.icon-wrapper {
+  width: 25px;
+  background-color: gainsboro;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+}
+
+#back_icon {
+  padding: 4px;
+  fill: #830542;
+  width: 25px;
+  height: 25px;
 }
 </style>
