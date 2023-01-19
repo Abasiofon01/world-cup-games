b0a574751c875d8e3e5238a4fc017749d82813e6
Abasiofon01 <abasiofonudonkang@gmail.com>
2023-01-19 14:43:35 +0100

    feat: add tooltip to edit icon.

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -4,10 +4,12 @@
     <p class="country">{{ teamData.name }}</p>
 
     <button
+      data-title="Edit"
+      id="btn"
       type="button"
       @click="$router.push({ name: 'edit_team', params: { id: teamData.id } })"
     >
-      <i class="uil uil-edit" title="edit"></i>
+      <i class="uil uil-edit"></i>
     </button>
   </div>
 </template>
@@ -33,7 +35,7 @@ export default {
 .img-and-country {
   display: flex;
   align-items: center;
-  background-color: white;
+  background-color: gainsboro;
   gap: 8px;
   border-radius: 3.34px;
   padding: 9px;
@@ -44,13 +46,50 @@ export default {
   height: 18px;
 }
 
+.country {
+  font-size: clamp(0.75rem, 0.6291rem + 0.7737vw, 1rem);
+  color: #830542;
+  font-weight: bold;
+}
+
 @media (max-width: 320px) {
   .img-and-country img {
     width: 20.75px;
     height: 14.3px;
   }
 }
-.img-and-country button {
-  justify-self: flex-end;
+.img-and-country #btn {
+  margin-left: auto;
+  color: #830542;
+}
+.img-and-country #btn:hover {
+  scale: 120%;
+  font-weight: 700;
+}
+
+[data-title]:hover:after {
+  opacity: 1;
+  transition: all 0.1s ease 0.5s;
+  visibility: visible;
+}
+[data-title]:after {
+  content: attr(data-title);
+  position: absolute;
+  top: -2.2em;
+  font-size: 10px;
+  right: 0;
+  padding: 0px 6px;
+  background-color: #830542;
+  color: gainsboro;
+  white-space: nowrap;
+  -moz-border-radius: 4px;
+  -webkit-border-radius: 4px;
+  border-radius: 4px;
+  opacity: 0;
+  z-index: 99999;
+  visibility: hidden;
+}
+[data-title] {
+  position: relative;
 }
 </style>
