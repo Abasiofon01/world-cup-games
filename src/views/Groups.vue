098217d3716f9f1c078663e08ed0f445e67219e9
Abasiofon01 <abasiofonudonkang@gmail.com>
2023-01-20 17:40:50 +0100

    feat: fetched and rendered all teams from the endpoint.

--- src/views/Groups.vue
+++ src/views/Groups.vue
@@ -7,19 +7,56 @@
     <template #title> Groups </template>
 
     <div class="groups">
-      <Group />
+      <div class="double-group">
+        <div v-for="group in allGroups" :key="group.name">
+          <p class="group-heading">group {{ group.name }}</p>
+          <div id="team">
+            <div
+              v-for="team in group.teams"
+              :key="team.name"
+              class="img-and-country"
+            >
+              <img :src="team.flag" :alt="team.name" />
+              <p class="country">{{ team.name }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </Layout>
 </template>
+
 <script>
-import Group from "../components/Group.vue";
 import Layout from "../Layout/Layout.vue";
+import { mapActions } from "vuex";
 
 export default {
   name: "Groups",
-  components: { Group, Layout },
+
+  components: { Layout },
+
+  data() {
+    return {
+      allGroups: {
+        type: Array,
+        required: true,
+      },
+    };
+  },
+
+  methods: {
+    ...mapActions(["fetchAllGroups"]),
+  },
+
+  async created() {
+    await this.fetchAllGroups().then((allGroupsData) => {
+      this.allGroups = allGroupsData.data;
+      console.log(this.allGroups);
+    });
+  },
 };
 </script>
+
 <style lang="css">
 .groups-wrapper {
   margin-top: 18px;
@@ -43,5 +80,33 @@ export default {
   .groups {
     gap: 6px;
   }
+
+  #team {
+    margin-top: 6px;
+  }
+}
+
+.double-group {
+  @apply grid grid-cols-2 gap-3;
+}
+
+@media (max-width: 375px) {
+  .double-group {
+    @apply grid-cols-1;
+  }
+}
+
+.group-heading {
+  font-size: clamp(0.75rem, 0.5687rem + 1.1605vw, 1.125rem);
+  color: white;
+  font-weight: bold;
+  text-transform: capitalize;
+}
+
+#team {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  margin-top: 12px;
 }
 </style>
