31b54e30984402bfee5149b7210d4fe1572b1d75
Abasiofon01 <abasiofonudonkang@gmail.com>
2023-01-25 19:53:52 +0100

    feat: create results component

---
+++ src/views/Results.vue
@@ -0,0 +1,44 @@
+<template>
+  <Layout
+    @create-new="$router.push({ name: 'new_result' })"
+    wrapperClass="results-wrapper"
+    :showBackBtn="false"
+  >
+    <template #title> Results </template>
+  </Layout>
+</template>
+
+<script>
+import Layout from "../Layout/Layout.vue";
+import { mapActions } from "vuex";
+
+export default {
+  name: "Results",
+
+  components: { Layout },
+
+  data() {
+    return {
+      allResults: {},
+    };
+  },
+
+  methods: {
+    ...mapActions(["fetchAllResults"]),
+  },
+
+  async created() {
+    await this.fetchAllResults().then((allResultsData) => {
+      console.log(allResultsData);
+      this.allResults = allResultsData.data;
+    });
+  },
+};
+</script>
+
+<style lang="css">
+.results-wrapper {
+  margin-top: 18px;
+  text-align: center;
+}
+</style>
