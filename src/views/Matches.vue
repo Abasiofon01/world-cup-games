490e61f7105fc565137f2ee56117a236e7655ab9
Abasiofon01 <abasiofonudonkang@gmail.com>
2023-01-25 20:32:52 +0100

    feat: update "matches" component with results and data from the backend

--- src/views/Matches.vue
+++ src/views/Matches.vue
@@ -7,63 +7,35 @@
     <template #title> Matches and results </template>
 
     <div class="matches">
-      <Match />
-
-      <div class="group-match-summary">
-        <p class="group-heading">Group A</p>
+      <div
+        class="group-match-summary"
+        v-for="group in groupedResults"
+        :key="`group_${group.name}`"
+      >
+        <p class="group-heading">Group {{ group.name }}</p>
         <div class="matches-wrapper">
           <p class="group-subheading">Matches</p>
-          <div class="result-wrapper">
+
+          <div
+            class="result-wrapper"
+            v-for="result in group.results"
+            :key="`match_${result.match_id}`"
+          >
             <div class="team-and-score-wrapper">
-              <div
-                class="img-and-country"
-                v-for="match in allMatches"
-                :key="`match_${match.id}`"
-              >
-                <!-- <img :src="match.teamOne.flag" :alt="match.teamOne.name" /> -->
-                <!-- <p class="country">{{ match.teamOne.name }}</p> -->
+              <div class="img-and-country">
+                <img :src="result.team1_flag" :alt="`${team1_flag} flag`" />
+
+                <p class="country">{{ result.team1 }}</p>
               </div>
-              <span class="score">-</span>
+              <span class="score">{{ result.team1_score }}</span>
             </div>
 
-            <!-- <div class="team-and-score-wrapper team-two-score-wrapper">
-              <span class="score">-</span>
-              <div v-for="team in team2" class="img-and-country">
-                <img :src="team.flag" :alt="team.country" />
-                <p class="country">{{ team.country }}</p>
+            <div class="team-and-score-wrapper team-two-score-wrapper">
+              <span class="score">{{ result.team2_score }}</span>
+              <div class="img-and-country">
+                <img :src="result.team2_flag" :alt="`${team2_flag} flag`" />
+                <p class="country">{{ result.team2 }}</p>
               </div>
-            </div> -->
-
-            <div class="btns-wrapper">
-              <button
-                data-title="Edit"
-                id="btn"
-                type="button"
-                v-for="id in matchById"
-                key="id"
-                @click="
-                  $router.push({
-                    name: 'edit_match',
-                    params: { id: id.id },
-                  })
-                "
-              >
-                <i class="uil uil-edit"></i>
-              </button>
-
-              <button
-                data-title="Edit"
-                id="btn"
-                type="button"
-                @click="
-                  $router.push({
-                    name: 'delete_match',
-                    params: { id: matchById },
-                  })
-                "
-              >
-                <i class="uil uil-trash-alt"></i>
-              </button>
             </div>
           </div>
         </div>
@@ -72,7 +44,6 @@
   </Layout>
 </template>
 <script>
-import Match from "../components/Match.vue";
 import Layout from "../Layout/Layout.vue";
 import { mapActions } from "vuex";
 
@@ -80,35 +51,66 @@ export default {
   names: "Matches",
 
   components: {
-    Match,
     Layout,
   },
 
   data() {
     return {
-      allMatches: {
-        // type: Array,
-        // required: true,
-      },
-
+      allResults: {},
+      groupedResults: [],
       matchById: {},
     };
   },
 
   methods: {
-    ...mapActions(["fetchAllMatches", "deleteMatch"]),
+    ...mapActions(["fetchAllResults"]),
   },
 
   async created() {
-    await this.fetchAllMatches().then((allMatchesData) => {
-      // console.log(allMatchesData);
-      this.allMatches = allMatchesData.data;
-      console.log(this.allMatches);
-      // this.matchById = this.allMatches.map((match) => {
-      //   return match;
-      // });
-      console.log(this.matchById);
+    await this.fetchAllResults().then((allResultsData) => {
+      this.allResults = allResultsData.data;
+      // console.log(this.allResults);
     });
+
+    this.groupedResults = Object.entries(
+      this.allResults.reduce(
+        (
+          acc,
+          {
+            group_name,
+            match_id,
+            match_date,
+            start_time,
+            team1,
+            team1_score,
+            team1_flag,
+            team2,
+            team2_score,
+            team2_flag,
+          }
+        ) => {
+          if (!acc[group_name]) {
+            acc[group_name] = [];
+          }
+          acc[group_name].push({
+            match_id,
+            match_date,
+            start_time,
+            team1,
+            team1_score,
+            team1_flag,
+            team2,
+            team2_score,
+            team2_flag,
+          });
+
+          return acc;
+        },
+        {}
+      )
+    ).map(([name, results]) => ({ name, results }));
+
+    // console.log(this.groupedResults);
   },
 };
 </script>
@@ -138,6 +140,7 @@ export default {
 }
 .group-heading {
   font-size: clamp(1.125rem, 0.9437rem + 1.1605vw, 1.5rem);
+  text-transform: capitalize;
   font-weight: bold;
 }
 
@@ -145,6 +148,10 @@ export default {
   display: flex;
   flex-direction: column;
   gap: 8px;
+}
+
+.matches-wrapper div {
+  display: flex;
 }
 .group-subheading {
   font-size: clamp(0.75rem, 0.5687rem + 1.1605vw, 1.125rem);
@@ -170,7 +177,7 @@ export default {
 .img-and-country {
   display: flex;
   align-items: center;
-  background-color: white;
+  background-color: gainsboro;
   gap: 7px;
   border-radius: 1.92px;
   padding: 8px 8px 8px 8px;
