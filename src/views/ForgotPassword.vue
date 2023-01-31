4dd6e9061fa2d7dccfef0359484d00a7bf28cdc3
Abasiofon01 <abasiofonudonkang@gmail.com>
2023-01-31 10:53:35 +0100

    feat: create forgot password component

---
+++ src/views/ForgotPassword.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="heading-and-form-wrapper">
+    <h1 class="heading">Forgot Password</h1>
+    <form>
+      <input type="email" name="email" placeholder="Email" required />
+
+      <button type="submit">Submit</button>
+      <div class="text-wrapper">
+        <p class="text">
+          Registered already?
+          <router-link class="link" :to="{ name: 'login' }">
+            Login now
+          </router-link>
+        </p>
+        <p class="text">
+          Not Registered?
+          <router-link class="link" :to="{ name: 'register' }">
+            Register
+          </router-link>
+        </p>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script></script>
