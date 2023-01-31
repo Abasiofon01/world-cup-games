c14ab8e2ab0374839aaf66065a3d78c069905f42
Abasiofon01 <abasiofonudonkang@gmail.com>
2023-01-31 10:54:03 +0100

    feat: create login component

---
+++ src/views/Login.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="heading-and-form-wrapper">
+    <h1 class="heading">Login</h1>
+    <form>
+      <input
+        v-model="form.username"
+        type="text"
+        name="user_name"
+        placeholder="Username"
+        required
+      />
+
+      <input
+        v-model="form.password"
+        type="password"
+        name="password"
+        placeholder="Password"
+        required
+      />
+
+      <button type="submit" @click.prevent="onSubmit">Login</button>
+      <div class="text-wrapper">
+        <p class="text">
+          Forgot Password?
+          <router-link class="link" :to="{ name: 'forgot-password' }">
+            Reset now
+          </router-link>
+        </p>
+        <p class="text">
+          Not Registered?
+          <router-link class="link" :to="{ name: 'register' }">
+            Register
+          </router-link>
+        </p>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+
+export default {
+  name: "Login",
+
+  data() {
+    return {
+      form: {
+        username: "",
+        password: "",
+      },
+    };
+  },
+
+  methods: {
+    onSubmit() {
+      this.signIn(this.form);
+      this.$router.replace({ name: "teams" });
+    },
+
+    ...mapActions({ signIn: "auth/signIn" }),
+  },
+};
+</script>
