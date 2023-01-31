0af8bf2e47408e60c2c69e254cd33216877e32da
Abasiofon01 <abasiofonudonkang@gmail.com>
2023-01-31 10:55:47 +0100

    feat: create reset password component

---
+++ src/views/ResetPassword.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="heading-and-form-wrapper">
+    <h1 class="heading">Reset Password</h1>
+    <form>
+      <input type="password" name="password" placeholder="Password" required />
+      <input type="password" name="password" placeholder="Password" required />
+
+      <button type="submit">Reset Password</button>
+      <div class="text-wrapper">
+        <p class="text">
+          Registered already?
+          <router-link class="link" :to="{ name: 'login' }">
+            Login now
+          </router-link>
+        </p>
+        <p class="text">
+          Not Registered?
+          <router-link class="link" :to="{ name: 'register' }">
+            Register
+          </router-link>
+        </p>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script></script>
