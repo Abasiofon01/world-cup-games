6cde9543a93215f5f435920d2a651ce79698b9b1
Abasiofon01 <abasiofonudonkang@gmail.com>
2023-01-31 10:45:07 +0100

    feat: dispatch user

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import "./style.css";
+import axios from "axios";
 import App from "./App.vue";
 import router from "./router";
 import { createApp } from "vue";
 import { store } from "./Store/store";
 
-const app = createApp(App);
-app.use(store);
-app.use(router);
-app.mount("#app");
+store.dispatch("auth/user").then(() => {
+  const app = createApp(App);
+  app.use(store);
+  app.use(router);
+  app.mount("#app");
+});
+
+// const app = createApp(App);
+// app.use(store);
+// app.use(router);
+// app.mount("#app");
